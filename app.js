1ecae9f4a54e79ab6a42875e34dce51a7b3bbb10
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-02-08 15:34:27 +0000

    test jest

---
+++ app.js
@@ -0,0 +1,29 @@
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+const fromDollarToYen = function(valueInDollar){
+    // convertimos el valor a dolares
+    let valueInYen = valueInDollar * 153.28;
+    // retornamos el valor
+    return valueInYen;
+}
+const fromYenToPound = function(valueInYen){
+    // convertimos el valor a dolares
+    let valueInPound = valueInYen * 102.32;
+    // retornamos el valor
+    return valueInPound;
+}
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
